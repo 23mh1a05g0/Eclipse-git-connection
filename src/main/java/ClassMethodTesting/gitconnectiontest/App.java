79eb2c3d1e36f9326ca60c199267c2c4f82c1f22
23mh1a05g0 <kaleshavalid2005@gmail.com>
2026-02-14 14:39:50 +0530

    First Commit

---
+++ src/main/java/ClassMethodTesting/gitconnectiontest/App.java
@@ -0,0 +1,10 @@
+package ClassMethodTesting.gitconnectiontest;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
